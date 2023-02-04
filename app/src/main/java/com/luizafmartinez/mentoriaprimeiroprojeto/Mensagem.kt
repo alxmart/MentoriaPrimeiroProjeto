3a98d76a640f1a0a027ca702565ac0212579d248
alxmart <alxmartinez@gmail.com>
2023-02-04 20:18:13 -0300

    Created classes Usuario (java) and Mensagem(Kotlin)

---
+++ app/src/main/java/com/luizafmartinez/mentoriaprimeiroprojeto/Mensagem.kt
@@ -0,0 +1,8 @@
+package com.luizafmartinez.mentoriaprimeiroprojeto
+
+class Mensagem {
+    fun enviarNovaMensagem() {
+        //val usuario = Usuario()
+       Usuario().autenticar()
+    }
+}
---
+++ app/src/main/java/com/luizafmartinez/mentoriaprimeiroprojeto/Usuario.java
@@ -0,0 +1,7 @@
+package com.luizafmartinez.mentoriaprimeiroprojeto;
+
+public class Usuario {
+    public void autenticar() {
+        new Mensagem().enviarNovaMensagem();
+    }
+}
