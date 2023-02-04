6dfa65e6e46dd5aee70f0578a89433a2cecb0f00
alxmart <alxmartinez@gmail.com>
2023-02-04 18:24:07 -0300

    Added fun clique and set onCLick attribute to call it.

--- app/src/main/java/com/luizafmartinez/mentoriaprimeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/mentoriaprimeiroprojeto/MainActivity.kt
@@ -2,11 +2,16 @@ package com.luizafmartinez.mentoriaprimeiroprojeto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
+
+    fun clique(view: View) {
+        println("Foi clicado")
+    }
+
 }
